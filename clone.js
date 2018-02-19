adc821221126552d38dd67087734e47000b2d6c2
senner007 <nielshtg@hotmail.com>
2018-02-19 12:50:17 +0100

    git init

---
+++ clone.js
@@ -0,0 +1,18 @@
+function cloneArrOrObj (arrObj) { // deep clone an array with potentially nested arrays or objects
+  return arrObj.slice(0).map(function (m) {
+   return Array.isArray(m) ? cloneArrOrObj(m) : typeof m === 'object' ? JSON.parse(JSON.stringify(m)) : m
+  }) 
+}
+
+function cloneArrOrObj_alt (arrObj) { // deep clone array or object
+  return JSON.parse(JSON.stringify(arrObj))
+}
+
+var arr = [1,2,[3,[5,2]],{3:{4:0}}]
+var cloned = cloneArrOrObj(arr);
+
+cloned[2][1][0] = 'Buller';
+cloned[3][3][4] = 'Buller';
+
+console.log(arr)
+console.log(cloned)
