a631b81d72ee27b7003fe7e171898a7370c4839e
senner007 <nielshtg@hotmail.com>
2018-02-19 12:52:43 +0100

    fix

--- clone.js
+++ clone.js
@@ -1,15 +1,15 @@
-function cloneArrOrObj (arrObj) { // deep clone an array with potentially nested arrays or objects
+function cloneArr (arrObj) { // deep clone an array with potentially nested arrays or objects
   return arrObj.slice(0).map(function (m) {
-   return Array.isArray(m) ? cloneArrOrObj(m) : typeof m === 'object' ? JSON.parse(JSON.stringify(m)) : m
+   return Array.isArray(m) ? cloneArr(m) : typeof m === 'object' ? JSON.parse(JSON.stringify(m)) : m
   }) 
 }
 
-function cloneArrOrObj_alt (arrObj) { // deep clone array or object
+function cloneArrOrObj (arrObj) { // deep clone array or object
   return JSON.parse(JSON.stringify(arrObj))
 }
 
 var arr = [1,2,[3,[5,2]],{3:{4:0}}]
-var cloned = cloneArrOrObj(arr);
+var cloned = cloneArr(arr);
 
 cloned[2][1][0] = 'Buller';
 cloned[3][3][4] = 'Buller';
